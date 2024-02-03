5e1bfb93e004382c2cfb8581b67641aa23f96ff4
xopclabs <github@conv2d.com>
2024-02-04 01:08:10 +0400

    Supporting direct lang change in-keyboard

--- app/src/behaviors/behavior_language.c
+++ app/src/behaviors/behavior_language.c
@@ -18,26 +18,53 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 
 struct behavior_lang_config {
     struct zmk_behavior_binding behavior;
-    uint8_t layer;
+    uint8_t layers_count;
+    uint8_t layers[];
 };
 
-struct behavior_lang_data {};
+struct behavior_lang_data {
+    uint8_t current_lang;
+};
 
-static int behavior_lang_init(const struct device *dev) { return 0; };
+static int behavior_lang_init(const struct device *dev) {
+    struct behavior_lang_data *data = dev->data;
+    data->current_lang = 0;
+    return 0;
+};
+
+static void lang_switch(const struct behavior_lang_config *config, struct behavior_lang_data *data,
+                        struct zmk_behavior_binding_event event, uint8_t target_lang) {
+    if (data->current_lang == target_lang)
+        return;
+    uint8_t number_of_switches = 0;
+    if (data->current_lang < target_lang) {
+        number_of_switches = target_lang - data->current_lang;
+    } else {
+        number_of_switches = config->layers_count - data->current_lang + target_lang;
+    }
+    LOG_DBG("LANG current_lang %d target_lang %d number_of_switches %d", data->current_lang,
+            target_lang, number_of_switches);
+    // Switch needed number of times
+    for (uint8_t i = 0; i < number_of_switches; i++) {
+        zmk_behavior_queue_add(event.position, config->behavior, true, 0);
+        zmk_behavior_queue_add(event.position, config->behavior, false, 0);
+        LOG_DBG("LANG switch");
+    }
+    data->current_lang = target_lang;
+    return;
+};
 
 static int lang_keymap_binding_pressed(struct zmk_behavior_binding *binding,
                                        struct zmk_behavior_binding_event event) {
     const struct device *dev = zmk_behavior_get_binding(binding->behavior_dev);
     const struct behavior_lang_config *config = dev->config;
+    struct behavior_lang_data *data = dev->data;
 
-    LOG_DBG("LANG position %d default_layer %d", event.position, zmk_keymap_layer_default());
-    LOG_DBG("LANG position %d config_layer %d", event.position, config->layer);
     LOG_DBG("LANG position %d config_layer_active %d", event.position,
-            zmk_keymap_layer_active(config->layer));
-    if (!zmk_keymap_layer_active(config->layer)) {
-        zmk_behavior_queue_add(event.position, config->behavior, true, 30);
-        zmk_behavior_queue_add(event.position, config->behavior, false, 0);
-        zmk_keymap_layer_to(config->layer);
+            zmk_keymap_layer_active(binding->param1));
+    if (!zmk_keymap_layer_active(binding->param1)) {
+        lang_switch(config, data, event, binding->param1 - 1);
+        zmk_keymap_layer_to(binding->param1);
     }
     return ZMK_BEHAVIOR_OPAQUE;
 }
@@ -55,7 +82,8 @@ static const struct behavior_driver_api behavior_lang_driver_api = {
     static struct behavior_lang_data behavior_lang_data_##n = {};                                  \
     static struct behavior_lang_config behavior_lang_config_##n = {                                \
         .behavior = ZMK_KEYMAP_EXTRACT_BINDING(0, DT_DRV_INST(n)),                                 \
-        .layer = DT_INST_PROP(n, layer),                                                           \
+        .layers = DT_INST_PROP(n, layers),                                                         \
+        .layers_count = DT_INST_PROP_LEN(n, layers),                                               \
     };                                                                                             \
     BEHAVIOR_DT_INST_DEFINE(n, behavior_lang_init, NULL, &behavior_lang_data_##n,                  \
                             &behavior_lang_config_##n, APPLICATION,                                \
